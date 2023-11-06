38e96d7adb4536f73d98738eaad8ca6d09fbd60f
Vinicius <vinicius@iconax.com.br>
2023-11-06 16:41:36 -0300

    Database configs and initaliazing Spring project

---
+++ src/test/java/com/security/introduction/SpringSecurityIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
